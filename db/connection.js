68bf3991ecc41bee4969a5972ee0df0278db8305
Mahamud Arteh <artehrush@gmail.com>
2021-08-30 21:45:14 +0100

    db configuration

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,23 @@
-const { Pool } = require('pg');
-const path = require('path');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const path = require("path");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: path.resolve(__dirname, `../.env.${ENV}`),
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 const config =
-ENV === 'production' 
-? {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-}
-: {};
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 module.exports = new Pool(config);
-
-
